6af0850a05cc883721780e4e2f05cdc64168f80e
Jordan Haskovec <jorhaskovec@gmail.com>
2016-06-19 18:17:05 -0500

    fill out the other pages

--- docker/flask/Site.py
+++ docker/flask/Site.py
@@ -4,8 +4,18 @@ APP = Flask(__name__)
 
 @APP.route('/guild')
 def guild():
-    '''Returns the index template'''
+    '''Returns the guild template'''
     return render_template('guild.html')
+
+@APP.route('/guild/info')
+def info():
+    '''Returns the info template'''
+    return render_template('info.html')
+
+@APP.route('/guild/apply')
+def apply():
+    '''Returns the info template'''
+    return render_template('apply.html')
 
 if __name__ == '__main__':
     APP.run(debug=True, host='0.0.0.0', port=80)
